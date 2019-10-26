0bfcf53b977579e5a83a5d7ace9a3ef40ce00775
Matteo Simonetti <matt.simonetti@hotmail.it>
2019-10-26 19:11:11 +0200

    Added while loop to easy restart the process

--- main.cpp
+++ main.cpp
@@ -17,44 +17,47 @@ bool isValidFile(const string& file)
 
 int main(int argc, int** argv)
 {
-	STARTUPINFO si;
-	PROCESS_INFORMATION pi;
-
-	ZeroMemory(&si, sizeof(si));
-	si.cb = sizeof(si);
-	ZeroMemory(&pi, sizeof(pi));
-
-	std::stringstream cmd;
-	cmd << "StarskyPC.exe /HWND=" << (int)GetConsoleHwnd() << " /SpawnerRespawn" << endl;
-
-	cout << "Ready to execute " << cmd.str() << endl;
-
-	system("pause");
-
-	CreateProcess(NULL, LPSTR(cmd.str().c_str()), NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi);
-
-	Sleep(50);//Give him some time. Otherwise it may not start
-
-	if (isValidFile("addon.dll"))
+	while (true)
 	{
-		cout << "Injecting.." << endl;
-		Inject(pi.dwProcessId, "addon.dll");
+		STARTUPINFO si;
+		PROCESS_INFORMATION pi;
+
+		ZeroMemory(&si, sizeof(si));
+		si.cb = sizeof(si);
+		ZeroMemory(&pi, sizeof(pi));
+
+		std::stringstream cmd;
+		cmd << "StarskyPC.exe /HWND=" << (int)GetConsoleHwnd() << " /SpawnerRespawn" << endl;
+
+		cout << "Ready to execute " << cmd.str() << endl;
+
+		system("pause");
+
+		CreateProcess(NULL, LPSTR(cmd.str().c_str()), NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi);
+
+		Sleep(50);//Give him some time. Otherwise it may not start
+
+		if (isValidFile("addon.dll"))
+		{
+			cout << "Injecting.." << endl;
+			Inject(pi.dwProcessId, "addon.dll");
+		}
+
+		//SuspendThread(pi.hThread);
+
+		//system("pause");
+
+		ResumeThread(pi.hThread);
+
+		WaitForSingleObject(pi.hProcess, INFINITE);
+
+		DWORD exitCode;
+		GetExitCodeProcess(pi.hProcess, &exitCode);
+
+		cout << "exit with " << exitCode << endl;
+		cout << "press any key to restart" << endl;
+
+		system("pause");
 	}
-
-	//SuspendThread(pi.hThread);
-
-	//system("pause");
-
-	ResumeThread(pi.hThread);
-
-	WaitForSingleObject(pi.hProcess, INFINITE);
-
-	DWORD exitCode;
-	GetExitCodeProcess(pi.hProcess, &exitCode);
-
-	cout << "exit with " << exitCode << endl;
-
-	system("pause");
-
 	return 0;
 }
